c45ca56726484edadf2229ffad7ecaf8621be41b
syedhussain75 <88985268+syedhussain75@users.noreply.github.com>
2021-10-07 14:27:14 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "shussain"
+    organization = "shussain"
     workspaces {
       name = "ace-iac-day-zero"
     }
